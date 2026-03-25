b9c7999a021682f9c73a161fad71c333d72ca82c
waablunder <waablunder@gmail.com>
2026-03-25 14:13:35 +0700

    Добавил _ в config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
 # Токен бота
 BOT_TOKEN = "8534805469:AAEUY93MrdpUNrfw85hHJsC4Js-EX1OpNKo"
 
-# Если хочешь через .env, раскомментируй эти строки:
-# BOT_TOKEN = os.getenv('BOT_TOKEN')
-# if not BOT_TOKEN:
-#     raise ValueError("Нет токена! Создайте .env файл с BOT_TOKEN")
+# Настройки базы данных
+DATABASE_NAME = "quiz_bot.db"
+
+# Настройки квиза
+QUESTIONS_PER_QUIZ = 10
+TIME_PER_QUESTION = 30
